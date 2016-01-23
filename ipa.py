7c858e12a95318eb57986d0ab907c4304c545063
Tomasz Maciejewski <tmaciejewsk@gmail.com>
2016-01-23 14:57:09 +0100

    save train schedule in db

--- ipa.py
+++ ipa.py
@@ -3,41 +3,67 @@
 import ipa_db
 import ipa_config
 import station
+import train
 import sys
 
-def create_db(db):
+def create_db(db, _):
     """creates new DB"""
     db.remove()
     db.create()
 
-def print_trains(db):
+def print_trains(db, _):
     """prints all trains from DB"""
     for train in db.get_trains():
         print train
 
-def visit_stations(db):
-    """visits stations and saves trains"""
+def get_trains_from_stations():
     trains = {}
 
     for station_id, station_name in ipa_config.stations:
-        print 'Visiting station', station_name
-        arrivals, departures = station.get_station(station_id)
+        try:
+            arrivals, _ = station.get_station(station_id)
+            print 'Visitied station', station_name
+        except:
+            print 'Cannot get trains from station', station_name
+
         for train in arrivals:
             trains[train[0]] = train
 
+    return trains
+
+def update_train(db, train):
+    try:
+        db.update_train(train[0], train[1], train[2], train[3], train[5])
+        print 'Updated train', train[0], train[1]
+    except Exception as e:
+        print 'Failed to update train', train[0], train[1], ':', e.messaeg
+
+def update_train_schedule(db, train_id):
+    try:
+        schedule = [(stop[3], stop[4], stop[5], stop[6], stop[7])
+                        for stop in train.get_train(train_id)]
+        db.update_schedule(train_id, schedule)
+        print 'Updated train schedule', train_id
+    except Exception as e:
+        print 'Failed to update train schedule', train_id, ':', e.message
+
+def update_trains(db, _):
+    """updated trains in DB based on preconfigured set of stations"""
+
+    trains = get_trains_from_stations()
     for train_id in trains:
-        try:
-            db.add_train(train_id,
-                         trains[train_id][1],
-                         trains[train_id][2],
-                         trains[train_id][3],
-                         trains[train_id][5])
-            print 'Added train', train_id
-        except:
-            print 'Failed to add train', train_id
+        update_train(db, trains[train_id])
+        update_train_schedule(db, train_id)
+
+def print_train(db, args):
+    """prints train all-time schedule"""
+    for train_id in db.get_train_schedules(args[0]):
+        for schedule_info in db.get_schedule_info(train_id):
+            print schedule_info
+        print
 
 if __name__ == "__main__":
-    commands = [create_db, print_trains, visit_stations]
+    commands = [create_db, print_trains, update_trains, print_train]
     command = None
 
     if len(sys.argv) > 1:
@@ -48,7 +74,8 @@ if __name__ == "__main__":
 
     if command:
         db = ipa_db.Db(ipa_config.db_name)
-        command(db)
+        command(db, sys.argv[2:])
     else:
+        print 'Commands:'
         for c in commands:
             print '\t', c.__name__, '--', c.__doc__
--- ipa_db.py
+++ ipa_db.py
@@ -17,6 +17,7 @@ class Db:
 
     def remove(self):
         self.__execute('DROP TABLE IF EXISTS trains')
+        self.__execute('DROP TABLE IF EXISTS schedule')
         self.__commit()
 
     def create(self):
@@ -27,14 +28,47 @@ class Db:
             train_date text,
             train_relation text
         )''')
+        self.__execute('''CREATE TABLE schedule(
+            train_id integer,
+            stop_id integer,
+            stop_name text,
+            sched_arrive_time text,
+            sched_arrive_delay text,
+            sched_departure_time text,
+            sched_departure_delay text
+        )''')
         self.__commit()
 
     def get_trains(self):
-        for row in self.__execute('SELECT * FROM trains'):
-            yield row
+        for row in self.__execute('SELECT DISTINCT train_number FROM trains'):
+            yield row[0]
 
-    def add_train(self, id, number, operator, date, relation):
+    def update_train(self, id, number, operator, date, relation):
+        self.__execute('DELETE FROM trains WHERE train_id = ?', (id,))
+
         self.__execute('''INSERT INTO trains VALUES (
-            ?, ?, ?, ?, ?
-        )''', (id, number, operator, date, relation))
+            ?, ?, ?, ?, ?)''',
+            (id, number, operator, date, relation)
+        )
+
         self.__commit()
+
+    def update_schedule(self, id, schedule):
+        self.__execute('DELETE FROM schedule WHERE train_id = ?', (id,))
+
+        stop_id = 1
+        for stop in schedule:
+            self.__execute('''INSERT INTO schedule VALUES (
+                ?, ?, ?, ?, ?, ?, ?)''',
+                (id, stop_id, stop[0], stop[1], stop[2], stop[3], stop[4])
+            )
+
+        self.__commit()
+
+    def get_train_schedules(self, name):
+        for row in self.__execute('SELECT train_id FROM trains WHERE train_number = ? ORDER BY train_id', (name,)):
+            yield row[0]
+
+    def get_schedule_info(self, id):
+        for row in self.__execute('SELECT * FROM schedule WHERE train_id = ? ORDER BY stop_id', (id,)):
+            yield row
