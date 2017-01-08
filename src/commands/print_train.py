13166a4a3efbda265c4985a99672410e88b9c5b5
Tomasz Maciejewski <tmaciejewsk@gmail.com>
2017-01-08 21:24:48 +0100

    prettier print_train command

--- src/commands/print_train.py
+++ src/commands/print_train.py
@@ -10,7 +10,14 @@ class PrintTrain:
             return
 
         for schedule in db.get_schedules(train_id[0]['train_id']):
-            print('-----------------------')
+            print('-' * 100)
+            print(schedule['schedule_date'])
+            print('-' * 100)
+
             for schedule_info in db.get_schedule_infos(schedule['schedule_id']):
-                print('\t|'.join([str(v) for v in schedule_info.values()]))
-            print
+                print('%30s | %20s | %5s min | %20s | %5s min' \
+                        % (schedule_info['station_name'], \
+                           str(schedule_info['arrival_time']), \
+                           str(schedule_info['arrival_delay']), \
+                           str(schedule_info['departure_time']), \
+                           str(schedule_info['departure_delay'])))
